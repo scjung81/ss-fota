9cf52e8edbb274e8731b60f2a8a9016fc08ce07a
automation pc <jungil.kwon@sk.com>
2020-12-03 13:01:23 +0900

    change connection_into file type (Excel -> CSV)

--- connection_info.py
+++ connection_info.py
@@ -3,5 +3,14 @@
 import pandas as pd
 
 def get_connection_info(key):
-    df = pd.read_excel("C:/sdqi_automation/connection_info.xlsx")
+    # df = pd.read_excel("C:/sdqi_automation/connection_info.xlsx")
+    df = pd.read_csv("C:/sdqi_automation/connection_info.csv",)
     return df.loc[df["key"] == key, "value"].values[0]
+
+
+if __name__ == "__main__":
+    # df = pd.read_excel("C:/sdqi_automation/connection_info.xlsx")
+    # df.to_csv("C:/sdqi_automation/connection_info.csv", index=False)
+
+    print(pd.read_csv("C:/sdqi_automation/connection_info.csv"))
+    print(get_connection_info("plm_host_test_server"))
