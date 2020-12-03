e51e15657a7dba3162024716d7b5fb21fbadc3cd
jungil.kwon <jungil.kwon@sk.com>
2020-12-03 13:24:54 +0900

    modify connection_info to read csv file as 'euc-kr' encoding

--- connection_info.py
+++ connection_info.py
@@ -3,14 +3,13 @@
 import pandas as pd
 
 def get_connection_info(key):
-    # df = pd.read_excel("C:/sdqi_automation/connection_info.xlsx")
-    df = pd.read_csv("C:/sdqi_automation/connection_info.csv",)
+    df = pd.read_csv("C:/sdqi_automation/connection_info.csv", encoding='euc-kr')
     return df.loc[df["key"] == key, "value"].values[0]
 
 
 if __name__ == "__main__":
     # df = pd.read_excel("C:/sdqi_automation/connection_info.xlsx")
-    # df.to_csv("C:/sdqi_automation/connection_info.csv", index=False)
+    # df.to_csv("C:/sdqi_automation/connection_info.csv", index=False, encoding='euc-kr')
 
-    print(pd.read_csv("C:/sdqi_automation/connection_info.csv"))
+    print(pd.read_csv("C:/sdqi_automation/connection_info.csv", encoding='euc-kr'))
     print(get_connection_info("plm_host_test_server"))
